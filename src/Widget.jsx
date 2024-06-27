56ad92403688cd8e47c8eaeb2bac6eb70d6b237c
ashraf <ashraf.alsamman@gmail.com>
2024-06-27 21:48:26 +0200

    adding hamburg

--- src/Widget.jsx
+++ src/Widget.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
  const Widget = () => {
   const widgetUrl = 'https://www.turbopass.com/rome-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=rome&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
-  const widgetUrl2 = 'https://www.turbopass.com/hamburg-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=rome&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
+  const widgetUrl2 = 'https://www.turbopass.com/hamburg-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=hamburg&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
 
   return (
     <div style={{ border: '1px solid #000', padding: '20px', width: '100%', height: '600px' }}>
