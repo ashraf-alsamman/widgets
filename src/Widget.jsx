399c9dc95f502fec2b3de68ee03cea0f6b51b264
ashraf <ashraf.alsamman@gmail.com>
2024-06-27 21:33:35 +0200

    adding roma

--- src/Widget.jsx
+++ src/Widget.jsx
@@ -1,11 +1,20 @@
-// src/Widget.js
+// src/Widget.jsx
 import React from 'react';
 
 const Widget = () => {
+  const widgetUrl = 'https://www.turbopass.com/rome-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=rome&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
+
   return (
-    <div style={{ border: '1px solid #000', padding: '20px', width: '300px' }}>
-      <h2>My Widget</h2>
-      <p>This is a simple widget.</p>
+    <div style={{ border: '1px solid #000', padding: '20px', width: '100%', height: '600px' }}>
+      <iframe
+        src={widgetUrl}
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+        title="PrestaShop Widget"
+      ></iframe>
     </div>
   );
 };
