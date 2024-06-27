c23bf4749f3f9a6c7b166c9658e6dac86f72813e
ashraf <ashraf.alsamman@gmail.com>
2024-06-27 21:57:47 +0200

    design update

--- src/Widget.jsx
+++ src/Widget.jsx
@@ -5,21 +5,21 @@ const Widget = () => {
   const widgetUrl2 = 'https://www.turbopass.com/hamburg-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=hamburg&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
 
   return (
-    <div style={{ border: '1px solid #000', padding: '20px', width: '50%', height: '400px' }}>
+    <div style={{ border: '1px solid #000', padding: '20px', width: '50%', height: '500px' }}>
       <iframe
         src={widgetUrl}
-        width="30%"
-        height="30%"
+        width="100%"
+        height="100%"
         frameBorder="0"
         allow="autoplay; encrypted-media"
         allowFullScreen
         title="PrestaShop Widget"
       ></iframe>
-
+      
       <iframe
         src={widgetUrl2}
-        width="30%"
-        height="30%"
+        width="100%"
+        height="100%"
         frameBorder="0"
         allow="autoplay; encrypted-media"
         allowFullScreen
