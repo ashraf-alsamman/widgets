40caee622cf02084c8ad7df08bef3a0354b464fc
ashraf <ashraf.alsamman@gmail.com>
2024-06-27 22:19:40 +0200

    design update

--- src/Widget.jsx
+++ src/Widget.jsx
@@ -1,30 +1,27 @@
 // src/Widget.jsx
 import React from 'react';
-const Widget = () => {
-  const widgetUrl = 'https://www.turbopass.com/rome-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=rome&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
-  const widgetUrl2 = 'https://www.turbopass.com/hamburg-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=hamburg&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
+// import Header from './Header';
+import './App.css'; // Import the CSS file
 
+const Widget = () => {
+  const widgetUrl = 'https://www.turbopass.com/rome-city-pass?landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=rome&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
+ 
   return (
-    <div style={{ border: '1px solid #000', padding: '20px', width: '50%', height: '500px' }}>
-      <iframe
-        src={widgetUrl}
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-        title="PrestaShop Widget"
-      ></iframe>
-      
-      <iframe
-        src={widgetUrl2}
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-        title="PrestaShop Widget"
-      ></iframe>
+    <div className="app-container">
+      {/* <Header /> */}
+      <div className="widget-container">
+        <iframe
+          width="100%"
+          height="100%"
+          src={widgetUrl}
+          className="widget"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          title="Rome City Pass"
+        ></iframe>
+ 
+      </div>
     </div>
   );
 };
