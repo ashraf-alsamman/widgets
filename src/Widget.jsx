0f1b5d92502d83aee5ae0cc28474901b06562333
ashraf <ashraf.alsamman@gmail.com>
2024-06-27 21:47:05 +0200

    adding hamburg

--- src/Widget.jsx
+++ src/Widget.jsx
@@ -1,13 +1,22 @@
 // src/Widget.jsx
 import React from 'react';
-
-const Widget = () => {
+ const Widget = () => {
   const widgetUrl = 'https://www.turbopass.com/rome-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=rome&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
+  const widgetUr2 = 'https://www.turbopass.com/hamburg-city-pass?widget-token=wptest12345&tokentp=&landing=&layout=embed&origin=https://tptesting.turbopass.com&frameId=869280980330052100&destination_id=&product=rome&option=&p=&currency=&_ga=null&r=&test=&order=&booking=&settlement=direct&locale=com&currency=&mobile=false';
 
   return (
     <div style={{ border: '1px solid #000', padding: '20px', width: '100%', height: '600px' }}>
       <iframe
         src={widgetUrl}
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+        title="PrestaShop Widget"
+      ></iframe>
+            <iframe
+        src={widgetUrl2}
         width="100%"
         height="100%"
         frameBorder="0"
